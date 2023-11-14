a5b3ab4867d64a49708d696ab1062ccbb8cb5edf
Kimi <kimilu@metropolia.fi>
2023-11-14 14:25:16 +0200

    Initial commit

--- moduuli2/tehtävä2.2/tehtävä2.2.js
+++ moduuli2/tehtävä2.2/tehtävä2.2.js
@@ -1,16 +1,22 @@
 'use strict';
 
 const ol = document.createElement('ol');
-ol.setAttribute("id","ol")
+ol.setAttribute("id","list")
+document.body.appendChild(ol)
+
 const participants = parseInt(prompt('Enter the number of participants.'));
+const namelist = [];
 
 for (let i = 0; i < participants; i++) {
   const name = prompt('Enter participant name.');
+  namelist.push(name)
+}
+namelist.sort();
+for (name of namelist){
   const li = document.createElement('li');
   li.innerHTML= name;
   ol.appendChild(li);
 }
-document.getElementById('list').appendChild(ol)
 
 
 
